a293762c7bd27a2e4e3e0a14779dadc4c2ec5402
abdennour <mail@abdennoor.com>
2020-03-21 02:50:11 +0300

    refactor: terraform and docker-compose for multi-env

---
+++ run-docker-compose.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# default or staging
+env=$(docker-compose run --rm terraform workspace show | tr -d '\r');
+
+docker-compose -f docker-compose.yaml -f docker-compose.${env}.yaml run --rm $@
