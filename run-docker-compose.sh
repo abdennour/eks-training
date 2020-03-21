2fe917ad8560c6b59e3efe18ef8e6f187258e57a
abdennour <mail@abdennoor.com>
2020-03-21 04:02:52 +0300

    feat: helmfile supports multi environments

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -2,5 +2,10 @@
 
 # default or staging
 env=$(docker-compose run --rm terraform workspace show | tr -d '\r');
+if [ $1 == "helmfile" ]; then
+shift;
+docker-compose -f docker-compose.yaml -f docker-compose.${env}.yaml run --rm helmfile --environment ${env} $@
+else
 
 docker-compose -f docker-compose.yaml -f docker-compose.${env}.yaml run --rm $@
+fi
