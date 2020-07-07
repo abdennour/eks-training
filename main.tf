6a7e7cedb32a70dcbc296b88782720abd1dfb736
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-07-07 15:56:45 -0700

    Migrate charts from helmfile.yml to main.tf as helm_release resources
    helmfile.yaml and other references to helmfile are no longer needed

--- main.tf
+++ main.tf
@@ -83,6 +83,16 @@ resource "kubernetes_namespace" "apps" {
   ]
 }
 
+resource "kubernetes_namespace" "monitoring" {
+  count = local.env == "default" ? 1 : 0
+  metadata {
+    name = "monitoring"
+  }
+  depends_on = [
+    module.eks.cluster_id
+  ]
+}
+
 provider "helm" {
   version        = "~> 1.2.3"
   kubernetes {
@@ -100,4 +110,86 @@ resource "helm_release" "hello-chart" {
   provisioner "local-exec" {
     command = "helm --kubeconfig kubeconfig_${module.eks.cluster_id} test -n ${self.namespace} ${self.name}"
   }
+}
+
+resource "helm_release" "metrics-server" {
+  count     = local.env == "default" ? 1 : 0
+  name      = "metrics-server"
+  chart     = "stable/metrics-server"
+  version   = "2.8.2"
+  namespace = "kube-system"
+
+  values    = [
+    "${file("./charts/metrics-server/values.yaml")}",
+  ]
+
+  provisioner "local-exec" {
+    command = "helm --kubeconfig kubeconfig_${module.eks.cluster_id} test -n ${self.namespace} ${self.name}"
+  }
+
+  depends_on = [
+    module.eks.cluster_id
+  ]
+}
+
+resource "helm_release" "prometheus" {
+  count   = local.env == "default" ? 1 : 0
+  name    = "prometheus"
+  chart   = "stable/prometheus-operator"
+  version = "8.13.11"
+  namespace = "monitoring"
+
+  set {
+    name  = "grafana.adminPassword"
+    value = var.GRAFANA_PWD
+  }
+  values    = [
+    "${file("./charts/prometheus/values.yaml")}",
+  ]
+  provisioner "local-exec" {
+    command = "helm --kubeconfig kubeconfig_${module.eks.cluster_id} test -n ${self.namespace} ${self.name}"
+  }
+
+  depends_on = [
+    module.eks.cluster_id
+  ]
+}
+
+resource "helm_release" "cluster-autoscaler" {
+  count     = local.env == "default" ? 1 : 0
+  name = "cluster-autoscaler"
+  chart = "stable/cluster-autoscaler"
+  version = "7.1.0"
+  namespace = "kube-system"
+  values    = [
+    "${file("./charts/metrics-server/cluster-autoscaler/values.yaml")}",
+  ]
+
+  provisioner "local-exec" {
+    command = "helm --kubeconfig kubeconfig_${module.eks.cluster_id} test -n ${self.namespace} ${self.name}"
+  }
+
+  depends_on = [
+    module.eks.cluster_id
+  ]
+}
+
+resource "helm_release" "ingress" {
+  name = "ingress"
+  chart = "stable/nginx-ingress"
+  version = "1.34.1"
+  namespace = "kube-system"
+  values    = [
+    "${file("./charts/nginx-ingress/values.yaml")}",
+    "${file("./charts/nginx-ingress/values.${local.env}.yaml")}"
+  ]
+  provisioner "local-exec" {
+    command = "helm --kubeconfig kubeconfig_${module.eks.cluster_id} test -n ${self.namespace} ${self.name}"
+  }
+
+  depends_on = [
+    module.eks.cluster_id
+  ]
+
+}
 }
